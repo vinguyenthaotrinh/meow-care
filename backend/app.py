fe346c38485acec0d52d7b6677432524b4d7b262
vinguyenthaotrinh <vinguyenthaotrinh@gmail.com>
2025-03-24 17:59:01 +0700

    feat: health check ❤️

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from src.routes import auth_bp, profile_bp, sleep_bp, hydrate_bp
+from src.routes import auth_bp, profile_bp, sleep_bp, hydrate_bp, health_bp
 from flask_jwt_extended import JWTManager
 import os
 
@@ -9,6 +9,7 @@ app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
 jwt = JWTManager(app)
 
 # Đăng ký routes từ folder routes/
+app.register_blueprint(health_bp, url_prefix="/health")
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(profile_bp, url_prefix="/profile")
 app.register_blueprint(sleep_bp, url_prefix="/sleep")
--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -1,6 +1,7 @@
+from .health_routes import health_bp
 from .auth_routes import auth_bp
 from .profile_routes import profile_bp
 from .sleep_routes import sleep_bp
 from .hydrate_routes import hydrate_bp
 
-__all__ = ["auth_bp", "profile_bp", "sleep_bp"]
+__all__ = ["health_bp", "auth_bp", "profile_bp", "sleep_bp"]
